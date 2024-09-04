3ef4de066571f8bdcdd5fa8bcb3b2b1812162e8a
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-09-04 20:16:49 -0300

    feat: animação das abas com frame-motion

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -55,7 +55,11 @@ export function SettingsTabs() {
           title="Integrações"
           isSelected={currentTab === 'tab9'}
         />
-        <TabItem value="tab9" title="API" isSelected={currentTab === 'tab9'} />
+        <TabItem
+          value="tab10"
+          title="API"
+          isSelected={currentTab === 'tab10'}
+        />
       </Tabs.List>
     </Tabs.Root>
   )
