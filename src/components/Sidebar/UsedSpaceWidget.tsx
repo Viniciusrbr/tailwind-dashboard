0370dd75ecf4f88f57d426311acb2bae70bebe41
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-09-30 17:28:19 -0300

    feat: Tema dark da sidebar

--- src/components/Sidebar/UsedSpaceWidget.tsx
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -1,24 +1,24 @@
 export function UsedSpaceWidget() {
   return (
-    <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+    <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5 dark:bg-zinc-800">
       <div className="space-y-1">
-        <span className="text-sm/5 font-medium text-violet-700">
+        <span className="text-sm/5 font-medium text-violet-700 dark:text-zinc-100">
           Espaço usado
         </span>
-        <p className="text-sm/5 text-violet-500">
+        <p className="text-sm/5 text-violet-500 dark:text-zinc-400">
           Sua equipe usou 80% do seu espaço disponível. Precisa de mais?
         </p>
       </div>
 
-      <div className="h-2 rounded-full bg-violet-100">
-        <div className="h-2 w-3/4 rounded-full bg-violet-600"></div>
+      <div className="h-2 rounded-full bg-violet-100 dark:bg-zinc-600">
+        <div className="h-2 w-3/4 rounded-full bg-violet-600 dark:bg-violet-400"></div>
       </div>
 
       <div className="space-x-3">
-        <button className="text-sm font-medium text-violet-500 hover:text-violet-700">
+        <button className="text-sm font-medium text-violet-500 hover:text-violet-700 dark:text-violet-300 dark:hover:text-violet-200">
           Ignorar
         </button>
-        <button className="text-sm font-medium text-violet-700 hover:text-violet-900">
+        <button className="text-sm font-medium text-violet-700 hover:text-violet-900 dark:text-zinc-300 dark:hover:text-zinc-100">
           Atualizar plano
         </button>
       </div>
